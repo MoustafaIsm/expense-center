20682b5d7ce27a1f27617f2ce0e491e0d5aa6568
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-25 19:33:36 +0300

    Admin-frontend => Added the font in the styles

--- expense-center-admin-frontend/src/pages/Login.jsx
+++ expense-center-admin-frontend/src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Input from '../components/common/Input';
 
 function Login() {
 
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('token', '12345');
+        navigate('/');
+    }
 
     return (
         <div className="bg-light-blue h-screen">
@@ -15,9 +21,9 @@ function Login() {
             </nav>
             <div className="flex flex-col justify-center items-center h-5/6">
                 <div className="bg-white p-8 border border-secondary-blue rounded-2xl w-2/5 flex flex-col gap-4">
-                    <p className="text-2xl font-bold"> Login </p>
+                    <p className="text-2xl bold-text"> Login </p>
                     <div className="w-full h-0.5 bg-secondary-blue"></div>
-                    <form className='flex flex-col gap-4'>
+                    <form className='flex flex-col gap-4' onSubmit={(e) => handleSubmit(e)}>
                         <Input
                             label='Email'
                             type='email'
@@ -30,7 +36,7 @@ function Login() {
                             value={password}
                             onChange={setPassword}
                             placeholder='Enter your password' />
-                        <button className='text-white bg-primary-blue uppercase'>
+                        <button className='text-white bg-primary-blue uppercase bold-text text-lg rounded-xl py-4 hover:bg-secondary-blue transition-all duration-300'>
                             Log in
                         </button>
                     </form>
