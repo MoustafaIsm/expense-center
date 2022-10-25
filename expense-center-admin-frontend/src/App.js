090fd5422bfb38414176d9a6ef7e29b858c23dfb
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-25 18:04:34 +0300

    Admin-frontend => Implemented the protective route in my routes

--- expense-center-admin-frontend/src/App.js
+++ expense-center-admin-frontend/src/App.js
@@ -5,13 +5,12 @@ import Statistics from './pages/Statistics';
 import Categories from './pages/Categories';
 import Feedback from './pages/Feedback';
 import Home from './pages/Home';
+import Protected from './components/Protected';
 
 function App() {
 
     const [token, setToken] = useState(localStorage.getItem('token') || '');
     const [isAuthenticated, setIsAuthenticated] = useState(token !== '' ? true : false);
-
-    console.log('token', token);
 
     useEffect(() => {
         if (token) {
@@ -22,9 +21,12 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to={isAuthenticated ? '/home' : '/login'} />} />
+                <Route path="/" element={<Navigate to='home' />} />
                 <Route path="/login" element={<h1>Login</h1>} />
-                <Route path='home' element={<Home />} >
+                <Route path='home' element={
+                    <Protected isAuthenticated={isAuthenticated}>
+                        <Home />
+                    </Protected>} >
                     <Route element={<Dashboard />} index />
                     <Route path="categories" element={<Categories />} />
                     <Route path="statistics" element={<Statistics />} />
--- expense-center-admin-frontend/src/components/Protected.jsx
+++ expense-center-admin-frontend/src/components/Protected.jsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 
 function Protected({ isAuthenticated, children }) {
     if (!isAuthenticated) {
-        return <Navigate to="/login" />;
+        return <Navigate to="/login" replace />;
     }
     return children;
 }
