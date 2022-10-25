bcb61614d8a9c857987757a985da760a5c9b539f
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-25 17:48:42 +0300

    Admin-frontend => Added a home page aand fixed the routes

--- expense-center-admin-frontend/src/App.js
+++ expense-center-admin-frontend/src/App.js
@@ -1,65 +1,36 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { DASHBOARD, CATEGORIES, STATISTICS, FEEDBACK } from './utilities/constants';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Statistics from './pages/Statistics';
 import Categories from './pages/Categories';
 import Feedback from './pages/Feedback';
-import SideNavButton from './components/SideNavButton';
+import Home from './pages/Home';
 
 function App() {
 
-    const [activePage, setActivePage] = useState(DASHBOARD);
+    const [token, setToken] = useState(localStorage.getItem('token') || '');
+    const [isAuthenticated, setIsAuthenticated] = useState(token !== '' ? true : false);
+
+    console.log('token', token);
+
+    useEffect(() => {
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, [token]);
 
     return (
         <BrowserRouter>
-            <div className='flex'>
-                <nav className="w-1/5 bg-primary-blue h-screen fixed min-w-200px">
-                    <ul className="py-5 flex flex-col">
-                        <li className='px-5 py-4'>
-                            <img src="/logo-with-side-text.png" alt="logo" />
-                        </li>
-                        <li>
-                            <Link to='/'>
-                                <SideNavButton
-                                    type={DASHBOARD}
-                                    active={activePage}
-                                    changeActive={setActivePage} />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/categories'>
-                                <SideNavButton
-                                    type={CATEGORIES}
-                                    active={activePage}
-                                    changeActive={setActivePage} />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/statistics'>
-                                <SideNavButton
-                                    type={STATISTICS}
-                                    active={activePage}
-                                    changeActive={setActivePage} />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/feedback'>
-                                <SideNavButton
-                                    type={FEEDBACK}
-                                    active={activePage}
-                                    changeActive={setActivePage} />
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/categories" element={<Categories />} />
-                    <Route path="/statistics" element={<Statistics />} />
-                    <Route path="/feedback" element={<Feedback />} />
-                </Routes>
-            </div >
+            <Routes>
+                <Route path="/" element={<Navigate to={isAuthenticated ? '/home' : '/login'} />} />
+                <Route path="/login" element={<h1>Login</h1>} />
+                <Route path='home' element={<Home />} >
+                    <Route element={<Dashboard />} index />
+                    <Route path="categories" element={<Categories />} />
+                    <Route path="statistics" element={<Statistics />} />
+                    <Route path="feedback" element={<Feedback />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     );
 }
--- expense-center-admin-frontend/src/utilities/constants.js
+++ expense-center-admin-frontend/src/utilities/constants.js
@@ -5,5 +5,13 @@ const STATISTICS = "Statistics";
 const CATEGORIES = "Categories";
 const FEEDBACK = "Feedback";
 
+const PAGE_TYPES = [DASHBOARD, STATISTICS, CATEGORIES, FEEDBACK];
 
-export { DASHBOARD, STATISTICS, CATEGORIES, FEEDBACK };
+
+export {
+    DASHBOARD,
+    STATISTICS,
+    CATEGORIES,
+    FEEDBACK,
+    PAGE_TYPES
+};
