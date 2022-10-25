12194a2aff4152dbf6523fcec8431c334e7a9c53
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-25 13:07:35 +0300

    Admin-frontend => Put the side navigation in the app.js file to use Link tag

--- expense-center-admin-frontend/src/App.js
+++ expense-center-admin-frontend/src/App.js
@@ -1,19 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SideNavigation from './components/SideNavigation';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
-        <div>
-            <SideNavigation />
-            <BrowserRouter>
+        <BrowserRouter>
+            <div className='flex'>
+                <nav className="w-1/5 bg-primary-blue h-screen">
+                    <ul className="py-5 flex flex-col">
+                        {/* Logo */}
+                        <li className='px-2'>
+                            <div>
+                                <img src="/logo-with-side-text.png" alt="logo" />
+                            </div>
+                        </li>
+                        {/* Links */}
+                        <li>
+                            <Link to='/'>
+                                <p>text</p>
+                            </Link>
+                        </li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
                 <Routes>
-                    {/* <Route path="/" element={<Dashboard />} />
-                    <Route path="/categories" element={<Categories />} />
+                    <Route path="/" element={<Dashboard />} />
+                    {/* <Route path="/categories" element={<Categories />} />
                     <Route path="/statistics" element={<Statistics />} />
                     <Route path="/feedback" element={<Feedback />} /> */}
                 </Routes>
-            </BrowserRouter>
-        </div>
+            </div >
+        </BrowserRouter>
     );
 }
 
---
+++ expense-center-admin-frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+function Dashboard() {
+    return (
+        <div className="">
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard
