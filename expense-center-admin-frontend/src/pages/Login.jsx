e9447e2524610da3344bfb32f3f2b8205a69bdb3
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-25 19:16:28 +0300

    Admin-frontend => Added a second input and add roboto font to the project

--- expense-center-admin-frontend/src/pages/Login.jsx
+++ expense-center-admin-frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
                 <img src="/logo-with-side-text.png" alt="logo" className="h-12" />
             </nav>
             <div className="flex flex-col justify-center items-center h-5/6">
-                <div className="bg-white p-8 border border-secondary-blue rounded-2xl w-1/2 flex flex-col gap-4">
+                <div className="bg-white p-8 border border-secondary-blue rounded-2xl w-2/5 flex flex-col gap-4">
                     <p className="text-2xl font-bold"> Login </p>
                     <div className="w-full h-0.5 bg-secondary-blue"></div>
                     <form className='flex flex-col gap-4'>
@@ -24,6 +24,15 @@ function Login() {
                             value={email}
                             onChange={setEmail}
                             placeholder='example@company.com' />
+                        <Input
+                            label='Password'
+                            type='password'
+                            value={password}
+                            onChange={setPassword}
+                            placeholder='Enter your password' />
+                        <button className='text-white bg-primary-blue uppercase'>
+                            Log in
+                        </button>
                     </form>
                 </div>
             </div>
