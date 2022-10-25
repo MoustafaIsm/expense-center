65447ab3e555ce332ab6e0f249893ad6399c63fc
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-25 22:03:24 +0300

    Admin-frontend => Added the functionality of clicking on each tab

--- expense-center-admin-frontend/src/components/tabs/Tabs.jsx
+++ expense-center-admin-frontend/src/components/tabs/Tabs.jsx
@@ -5,12 +5,20 @@ function Tabs() {
 
     const [activeTab, setActiveTab] = useState('not-banned');
 
+    const openBanned = () => {
+        setActiveTab('banned');
+    }
+
+    const openNotBanned = () => {
+        setActiveTab('not-banned');
+    }
+
     return (
         <div className="w-full">
             {/* Tabs */}
             <ul className="w-full flex text-center text-lg border-b border-primary-blue">
-                <li className={`w-1/2 py-3 ${activeTab === 'not-banned' ? 'bg-primary-blue text-white underline underline-offset-8' : ''}`}> Not banned </li>
-                <li className={`w-1/2 py-3 ${activeTab !== 'not-banned' ? 'bg-primary-blue text-white underline underline-offset-8' : ''}`}> Banned </li>
+                <li className={`w-1/2 py-3 transition-all duration-300 ${activeTab === 'not-banned' ? 'bg-primary-blue text-white underline underline-offset-8' : 'hover:cursor-pointer hover:bg-secondary-blue hover:text-white'}`} onClick={openNotBanned}> Not banned </li>
+                <li className={`w-1/2 py-3 transition-all duration-300 ${activeTab !== 'not-banned' ? 'bg-primary-blue text-white underline underline-offset-8' : 'hover:cursor-pointer hover:bg-secondary-blue hover:text-white'}`} onClick={openBanned}> Banned </li>
             </ul>
             {/* Tab Content */}
             <div>
