650feb85ece06c74c424a3b2eedbbeba11357570
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-26 13:43:14 +0300

    Admin-frontend => Added an input to the add category modal

--- expense-center-admin-frontend/src/components/common/Input.jsx
+++ expense-center-admin-frontend/src/components/common/Input.jsx
@@ -1,5 +1,5 @@
 
-function Input({ label, type = 'text', inputRef, placeholder }) {
+function Input({ label, type, inputRef, placeholder }) {
     return (
         <div className="flex flex-col">
             <label htmlFor="name" className="mb-2">
@@ -12,6 +12,10 @@ function Input({ label, type = 'text', inputRef, placeholder }) {
                 className="border-2 border-primary-blue rounded-xl p-5 text-lg focus:border-2 focus:border-secondary-blue focus:outline-none" />
         </div>
     )
+}
+
+Input.defaultProps = {
+    type: 'text',
 }
 
 export default Input
--- expense-center-admin-frontend/src/components/modals/AddCategoryModal.jsx
+++ expense-center-admin-frontend/src/components/modals/AddCategoryModal.jsx
@@ -1,7 +1,11 @@
+import { useRef } from 'react';
 import Dialog from '@mui/material/Dialog';
 import { DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import Input from '../common/Input';
 
 function AddCategoryModal({ title, onClose, ...props }) {
+
+    const categoryNameRef = useRef();
 
     const handleSubmit = () => {
         // TODO: Do an API call to add the category
@@ -13,7 +17,20 @@ function AddCategoryModal({ title, onClose, ...props }) {
             <DialogTitle className='bold-text'> {title} </DialogTitle>
 
             <DialogContent className='text-black'>
+                <form>
+                    <Input
+                        label={'Name'}
+                        inputRef={categoryNameRef}
+                        placeholder={'Category name'} />
+                    {/* Sub categories */}
 
+                    <button
+                        type='submit'
+                        onClick={handleSubmit}
+                        className="bg-primary-green text-lg text-white uppercase bold-text hover:cursor-pointer hover:bg-secondary-green py-2 px-4 rounded-xl transition-all duration-300">
+                        Add
+                    </button>
+                </form>
             </DialogContent>
 
             <DialogActions>
