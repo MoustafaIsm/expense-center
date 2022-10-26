9f035f017a8ef0949a6f378a285050bb5508079e
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-26 16:51:12 +0300

    Admin-frontend => Added the contents of the feedback card

--- expense-center-admin-frontend/src/components/feedback/FeedbackCard.jsx
+++ expense-center-admin-frontend/src/components/feedback/FeedbackCard.jsx
@@ -1,7 +1,28 @@
 
 function FeedbackCard() {
     return (
-        <div>FeedbackCard</div>
+        <div className="bg-white boder border-x-secondary-blue shadow-md shadow-secondary-blue p-6">
+            <div className="flex gap-4 items-center justify-between">
+                <div className="flex gap-4 items-center">
+                    <div>
+                        <img className="rounded-full h-16 w-16" src="/profile-picture-placeholder.jpg" alt="Profile" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p className="bold-text">Username</p>
+                        <p>email</p>
+                    </div>
+                </div>
+                <div>
+                    <p>Date</p>
+                </div>
+            </div>
+            <div>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio sequi qui eius! Cupiditate similique recusandae consectetur dolor facilis, placeat dolore eaque laboriosam natus ex eligendi suscipit tempore adipisci exercitationem quo.
+                    Reiciendis quam sint, rerum quos aliquam suscipit perferendis consequatur similique, laborum iusto odit amet, incidunt illo qui veritatis. Fuga nihil voluptatum earum hic unde laborum at fugit, mollitia distinctio nostrum?
+                </p>
+            </div>
+        </div>
     )
 }
 
