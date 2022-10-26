7df073668bc7be3a17b8faca141950ece46df6f3
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-26 11:22:35 +0300

    Admin-frontend => Created a new category graph component

---
+++ expense-center-admin-frontend/src/components/graphs/CategoryGraph.jsx
@@ -0,0 +1,8 @@
+
+function CategoryGraph() {
+    return (
+        <div>CategoryGraph</div>
+    )
+}
+
+export default CategoryGraph
--- expense-center-admin-frontend/src/pages/Categories.jsx
+++ expense-center-admin-frontend/src/pages/Categories.jsx
@@ -1,10 +1,16 @@
 import Layout from "../components/layouts/Layout";
 import { CATEGORIES } from '../utilities/constants';
+import CategoryGraph from "../components/graphs/CategoryGraph";
 
 function Categories() {
+
+
+
     return (
         <Layout title={CATEGORIES}>
-
+            <div>
+                <CategoryGraph />
+            </div>
         </Layout>
     )
 }
