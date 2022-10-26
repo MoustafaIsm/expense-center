a1e33a66cc9c46a083175651ec5c1291140fe558
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-26 13:17:07 +0300

    Admin-frontend => Fixed the layout to have a button when needed

--- expense-center-admin-frontend/src/components/layouts/layout.js
+++ expense-center-admin-frontend/src/components/layouts/layout.js
@@ -1,9 +1,17 @@
 
-function Layout({ title, children }) {
+function Layout({ title, hasButton = false, buttonText = '', children }) {
     return (
         <div className="p-9 ml-20% w-full">
-            <header className="mb-9">
+            <header className="mb-9 flex justify-between items-center">
                 <p className="bold-text text-2xl">{title}</p>
+                {
+                    hasButton ?
+                        <button className="bg-primary-green text-lg text-white uppercase bold-text hover:cursor-pointer hover:bg-secondary-green py-2 px-4 rounded-xl transition-all duration-300">
+                            {buttonText}
+                        </button>
+                        :
+                        <></>
+                }
             </header>
             {/* Page content */}
             <main className="w-full">{children}</main>
--- expense-center-admin-frontend/src/pages/Categories.jsx
+++ expense-center-admin-frontend/src/pages/Categories.jsx
@@ -7,7 +7,7 @@ function Categories() {
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return (
-        <Layout title={CATEGORIES}>
+        <Layout title={CATEGORIES} hasButton={true} buttonText={'Add category'}>
             <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
                 {
                     array.map((category, index) => (
