c55e5fa0423116936919c175389cc0edf965c3f1
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-26 11:56:20 +0300

    Admin-frontend => Added the contents with dummy data of the category graph component

--- expense-center-admin-frontend/src/components/graphs/CategoryGraph.jsx
+++ expense-center-admin-frontend/src/components/graphs/CategoryGraph.jsx
@@ -1,7 +1,33 @@
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
 
 function CategoryGraph() {
+
+    ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
+
+    const options = {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Category name',
+            },
+        },
+    };
+
+    const labels = ['sub1', 'sub2', 'sub3', 'sub4', 'sub5'];
+
+    const data = {
+        labels,
+        datasets: [{
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+            backgroundColor: labels.map(() => faker.internet.color()),
+        },],
+    };
+
     return (
-        <div>CategoryGraph</div>
+        <Bar options={options} data={data} />
     )
 }
 
