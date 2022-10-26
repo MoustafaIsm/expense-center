b43dc59368c9b75cc4d979bace05a01e572c0af6
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-26 07:57:34 +0300

    Admin-frontend => Added the table header in the tab component

--- expense-center-admin-frontend/src/components/tabs/Tab.jsx
+++ expense-center-admin-frontend/src/components/tabs/Tab.jsx
@@ -1,8 +1,20 @@
 
 function Tab({ text }) {
+
+
+
     return (
-        <div>
-            {text}
+        <div className="w-full">
+            <table className="w-full">
+                <tr>
+                    <th className="w-1/5 border-l border-b border-gray-300 bold-text"> Username </th>
+                    <th className="w-1/5 border-l border-b border-gray-300 bold-text"> Email </th>
+                    <th className="w-1/5 border-l border-b border-gray-300 bold-text"> Email Verified </th>
+                    <th className="w-1/5 border-l border-b border-gray-300 bold-text"> Is Admin </th>
+                    <th className="w-1/5 border-l border-b border-gray-300"></th>
+                </tr>
+
+            </table>
         </div>
     )
 }
