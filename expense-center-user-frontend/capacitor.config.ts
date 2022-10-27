5c346fd1a774cb4898f6aa9ac5cb1bc415b2ded9
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-27 17:34:07 +0300

    User-frontend => Created an ionic with angular project for the user

---
+++ expense-center-user-frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'expense-center-user-frontend',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
