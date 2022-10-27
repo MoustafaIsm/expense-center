76737d6ef267de90aa112e23d64131bfd4daa647
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-27 20:24:47 +0300

    User-frontend => Generated a home page for my main page

--- expense-center-user-frontend/src/app/pages/main/main-routing.module.ts
+++ expense-center-user-frontend/src/app/pages/main/main-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: MainPage
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
