c8ca145dd015a97138a7b0964719ffde22165e3d
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-27 21:16:30 +0300

    User-frontend => Generated a card component for the feeds

---
+++ expense-center-user-frontend/src/app/conponents/user-info-card/user-info-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-card',
+  templateUrl: './user-info-card.component.html',
+  styleUrls: ['./user-info-card.component.scss'],
+})
+export class UserInfoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
