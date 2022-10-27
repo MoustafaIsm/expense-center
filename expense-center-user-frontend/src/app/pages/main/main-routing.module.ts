7a102c9e061535b5291d8d00d00d3d8ac5f99d5a
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-27 20:52:26 +0300

    User-frontend => Added the  routes of the main page with the buttom tabs

--- expense-center-user-frontend/src/app/pages/main/main-routing.module.ts
+++ expense-center-user-frontend/src/app/pages/main/main-routing.module.ts
@@ -6,28 +6,41 @@ import { MainPage } from './main.page';
 const routes: Routes = [
   {
     path: '',
-    component: MainPage
+    component: MainPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'messages',
+        loadChildren: () => import('./messages/messages.module').then(m => m.MessagesPageModule)
+      },
+      {
+        path: 'favorites',
+        loadChildren: () => import('./favorites/favorites.module').then(m => m.FavoritesPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/main/home',
+        pathMatch: 'full'
+      },
+    ]
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: '/main/home',
+    pathMatch: 'full'
   },
-  {
-    path: 'messages',
-    loadChildren: () => import('./messages/messages.module').then( m => m.MessagesPageModule)
-  },
-  {
-    path: 'favorites',
-    loadChildren: () => import('./favorites/favorites.module').then( m => m.FavoritesPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class MainPageRoutingModule {}
+export class MainPageRoutingModule { }
