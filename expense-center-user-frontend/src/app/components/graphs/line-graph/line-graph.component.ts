cd60aa6a8b6b5ab41c47a87de71d6c439a9c60d7
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-28 21:24:48 +0300

    User-frontend => Added some more styles for the graph

--- expense-center-user-frontend/src/app/components/graphs/line-graph/line-graph.component.ts
+++ expense-center-user-frontend/src/app/components/graphs/line-graph/line-graph.component.ts
@@ -26,14 +26,17 @@ export class LineGraphComponent implements OnInit, AfterViewInit {
       data: {
         labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
         datasets: [{
-          label: 'Viewers in millions',
+          label: 'Savings',
           data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
-          backgroundColor: 'rgb(38, 194, 129, 0.5)', // array should have same number of elements as number of dataset
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          backgroundColor: 'rgb(139, 197, 190, 0.5)',
+          borderColor: 'rgb(0, 109, 119)',
           borderWidth: 1
         }]
       },
       options: {
+        legend: {
+          display: false
+        },
         scales: {
           yAxes: [{
             ticks: {
