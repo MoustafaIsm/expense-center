7d4754a2f72a225ea3c69fcde029ab0226f73d76
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-28 16:44:32 +0300

    User-frontend => Added properties and methods for the register page

--- expense-center-user-frontend/src/app/pages/register/register.module.ts
+++ expense-center-user-frontend/src/app/pages/register/register.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RegisterPageRoutingModule } from './register-routing.module';
+import {
+  ComponentsModule
+} from 'src/app/components/components.module';
 
 import { RegisterPage } from './register.page';
 
@@ -13,8 +14,9 @@ import { RegisterPage } from './register.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RegisterPageRoutingModule
+    RegisterPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [RegisterPage]
 })
-export class RegisterPageModule {}
+export class RegisterPageModule { }
--- expense-center-user-frontend/src/app/pages/register/register.page.ts
+++ expense-center-user-frontend/src/app/pages/register/register.page.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  email: string;
+  password: string;
+  dateOfBirth: Date;
+  gender: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeEmail(newValue: string) {
+    this.email = newValue;
+  }
+
+  changePassword(newValue: string) {
+    this.password = newValue;
+  }
+
+  changeDateOfBirth(newValue: Date) {
+    this.dateOfBirth = newValue;
+  }
+
+  changeGender(newValue: string) {
+    this.gender = newValue;
   }
 
 }
