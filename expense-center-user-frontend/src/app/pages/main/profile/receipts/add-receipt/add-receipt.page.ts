3d3ead0e3b1011eb5fc3c13a453a73b1f1230669
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 12:32:21 +0300

    User-frontend => Added some properties and meethods for the add receipt page

--- expense-center-user-frontend/src/app/pages/main/profile/receipts/add-receipt/add-receipt.page.ts
+++ expense-center-user-frontend/src/app/pages/main/profile/receipts/add-receipt/add-receipt.page.ts
@@ -6,13 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-receipt.page.scss'],
 })
 export class AddReceiptPage implements OnInit {
+  title: string;
+  date: Date;
+  amount: number;
   category: string;
-  categoryTypes = ['Income', 'Outcome'];
+  receiptType: string;
   receiptFile: string;
+  categoryTypes = ['Income', 'Outcome'];
+  receiptTypes = ['Income', 'Outcome'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeTitle(value: string) {
+    this.title = value;
+  }
+
+  changeDate(value: Date) {
+    this.date = value;
+  }
+
+  changeAmount(value: number) {
+    this.amount = value;
   }
 
   changeCategory(value: string) {
