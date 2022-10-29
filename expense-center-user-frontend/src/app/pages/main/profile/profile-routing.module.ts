1a30ef89987ad9d6819926294c41a23def87f3e5
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 08:33:56 +0300

    User-frotend => Generated an edit profile page

--- expense-center-user-frontend/src/app/pages/main/profile/profile-routing.module.ts
+++ expense-center-user-frontend/src/app/pages/main/profile/profile-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: ProfilePage
+  },
+  {
+    path: 'edit-profile',
+    loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
   }
 ];
 
