3204b14051b19035df951ef27b471583b1fb1cc0
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 09:35:04 +0300

    User-frontend => Added the receipts card to the outcome page

--- expense-center-user-frontend/src/app/pages/main/profile/receipts/outcome-receipts/outcome-receipts.module.ts
+++ expense-center-user-frontend/src/app/pages/main/profile/receipts/outcome-receipts/outcome-receipts.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { OutcomeReceiptsPageRoutingModule } from './outcome-receipts-routing.module';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { OutcomeReceiptsPage } from './outcome-receipts.page';
 
@@ -13,8 +12,9 @@ import { OutcomeReceiptsPage } from './outcome-receipts.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    OutcomeReceiptsPageRoutingModule
+    OutcomeReceiptsPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [OutcomeReceiptsPage]
 })
-export class OutcomeReceiptsPageModule {}
+export class OutcomeReceiptsPageModule { }
