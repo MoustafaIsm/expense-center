55365b811f842e8b96a10e634b4aa7132a01bf77
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 20:13:54 +0300

    User-frontend => Refractored the add receipt page header to use the component

--- expense-center-user-frontend/src/app/pages/main/profile/receipts/add-receipt/add-receipt.module.ts
+++ expense-center-user-frontend/src/app/pages/main/profile/receipts/add-receipt/add-receipt.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AddReceiptPageRoutingModule } from './add-receipt-routing.module';
+
 import { ComponentsModule } from 'src/app/components/components.module';
+import { MainPagesHeaderModule } from 'src/app/components/main-pages-header/main-pages-header.module';
 
 import { AddReceiptPage } from './add-receipt.page';
 
@@ -13,6 +15,7 @@ import { AddReceiptPage } from './add-receipt.page';
     FormsModule,
     IonicModule,
     AddReceiptPageRoutingModule,
+    MainPagesHeaderModule,
     ComponentsModule
   ],
   declarations: [AddReceiptPage]
