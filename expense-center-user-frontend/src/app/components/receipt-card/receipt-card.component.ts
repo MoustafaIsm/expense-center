5a2f27588e78e9ebc56e605b8bb2da47c250634f
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 09:19:25 +0300

    User-frontend => Generated a receipt card component

---
+++ expense-center-user-frontend/src/app/components/receipt-card/receipt-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipt-card',
+  templateUrl: './receipt-card.component.html',
+  styleUrls: ['./receipt-card.component.scss'],
+})
+export class ReceiptCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
