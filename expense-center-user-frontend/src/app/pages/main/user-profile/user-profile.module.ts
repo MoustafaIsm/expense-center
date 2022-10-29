1f380a6751259bd19584f967611d2c94e365bae7
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 20:07:49 +0300

    User-frontend => Refractored the user profile page header to use the component

--- expense-center-user-frontend/src/app/pages/main/user-profile/user-profile.module.ts
+++ expense-center-user-frontend/src/app/pages/main/user-profile/user-profile.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { UserProfilePageRoutingModule } from './user-profile-routing.module';
+
 import { ComponentsModule } from 'src/app/components/components.module';
 import { GraphsModule } from 'src/app/components/graphs/graphs.module';
+import { MainPagesHeaderModule } from 'src/app/components/main-pages-header/main-pages-header.module';
 
 import { UserProfilePage } from './user-profile.page';
 
@@ -14,6 +16,7 @@ import { UserProfilePage } from './user-profile.page';
     FormsModule,
     IonicModule,
     UserProfilePageRoutingModule,
+    MainPagesHeaderModule,
     ComponentsModule,
     GraphsModule
   ],
