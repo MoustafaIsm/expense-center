b0fec41bafef1c6241f537f004888627c8a5374e
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 20:09:57 +0300

    User-frontend => Refractored the edit profile page header to use the component

--- expense-center-user-frontend/src/app/pages/main/profile/edit-profile/edit-profile.module.ts
+++ expense-center-user-frontend/src/app/pages/main/profile/edit-profile/edit-profile.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { EditProfilePageRoutingModule } from './edit-profile-routing.module';
+
 import { ComponentsModule } from 'src/app/components/components.module';
+import { MainPagesHeaderModule } from 'src/app/components/main-pages-header/main-pages-header.module';
 
 import { EditProfilePage } from './edit-profile.page';
 
@@ -13,6 +15,7 @@ import { EditProfilePage } from './edit-profile.page';
     FormsModule,
     IonicModule,
     EditProfilePageRoutingModule,
+    MainPagesHeaderModule,
     ComponentsModule
   ],
   declarations: [EditProfilePage]
