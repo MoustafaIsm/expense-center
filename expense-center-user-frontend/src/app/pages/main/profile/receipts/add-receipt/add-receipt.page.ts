c22489ad4b6968527734ab54d02a3e0595b82a8e
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 12:28:27 +0300

    User-frontend => Made the add receipt page receive the value of the input

--- expense-center-user-frontend/src/app/pages/main/profile/receipts/add-receipt/add-receipt.page.ts
+++ expense-center-user-frontend/src/app/pages/main/profile/receipts/add-receipt/add-receipt.page.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AddReceiptPage implements OnInit {
   category: string;
   categoryTypes = ['Income', 'Outcome'];
+  receiptFile: string;
 
   constructor() { }
 
@@ -16,6 +17,10 @@ export class AddReceiptPage implements OnInit {
 
   changeCategory(value: string) {
     this.category = value;
+  }
+
+  changeReceiptFile(value: string) {
+    this.receiptFile = value;
   }
 
 }
