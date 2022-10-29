fa1a337c0c48bab28def1461958f168ead8d9d8c
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 10:26:29 +0300

    User-frontend => Generated a dropdownl ist component

---
+++ expense-center-user-frontend/src/app/components/common/dropdown-list/dropdown-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-list',
+  templateUrl: './dropdown-list.component.html',
+  styleUrls: ['./dropdown-list.component.scss'],
+})
+export class DropdownListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
