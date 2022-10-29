f44e198b15e3480e822a5b76ba4439f4d35773c3
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 20:04:31 +0300

    User-frontend => Refractored the messages page header to use the component

--- expense-center-user-frontend/src/app/pages/main/messages/messages.module.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/messages.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { MessagesPageRoutingModule } from './messages-routing.module';
+
 import { ComponentsModule } from 'src/app/components/components.module';
+import { MainPagesHeaderModule } from 'src/app/components/main-pages-header/main-pages-header.module';
 
 import { MessagesPage } from './messages.page';
 
@@ -13,6 +15,7 @@ import { MessagesPage } from './messages.page';
     FormsModule,
     IonicModule,
     MessagesPageRoutingModule,
+    MainPagesHeaderModule,
     ComponentsModule
   ],
   declarations: [MessagesPage]
