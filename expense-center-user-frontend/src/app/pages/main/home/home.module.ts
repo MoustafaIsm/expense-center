9ddc81925707879e3f3bdea56b1fc34085bfcffe
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-29 20:00:27 +0300

    User-frontend => Refractored the home page header to use the component

--- expense-center-user-frontend/src/app/pages/main/home/home.module.ts
+++ expense-center-user-frontend/src/app/pages/main/home/home.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
+
 import { ComponentsModule } from 'src/app/components/components.module';
+import { MainPagesHeaderModule } from 'src/app/components/main-pages-header/main-pages-header.module';
 
 import { HomePage } from './home.page';
 
@@ -14,6 +16,7 @@ import { HomePage } from './home.page';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
+    MainPagesHeaderModule,
     ComponentsModule
   ],
   declarations: [HomePage]
