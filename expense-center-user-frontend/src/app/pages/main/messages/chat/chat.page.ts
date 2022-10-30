89177824e49bf4fac944b24d5cb0c3d4e773cabd
Moustafa Ismail <moustafaism013@hotmail.com>
2022-10-30 16:15:02 +0200

    User-frontend => Added style for the slef message

--- expense-center-user-frontend/src/app/pages/main/messages/chat/chat.page.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/chat/chat.page.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ChatPage implements OnInit {
 
   message = {
-    text: 'hi',
+    text: 'hiasdasdgahsgdbhasdhasdavhgsdvaghsvdhiasdasdgahsgdbhasdhasdavhgsdvaghsvd',
     id: 1,
-    date: '2019-01-01',
+    date: '2:30 PM',
   };
   id = 1;
 
