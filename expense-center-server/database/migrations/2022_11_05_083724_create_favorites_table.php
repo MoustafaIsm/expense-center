094337665f744733310a161b09e0351e7f12a9f8
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-05 10:38:47 +0200

    Backend => Created favorites table migration

---
+++ expense-center-server/database/migrations/2022_11_05_083724_create_favorites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->integer('user_id')->references('id')->on('users');
+            $table->integer('favorited_id')->references('id')->on('users');
+            $table->timestamps();
+
+            $table->primary(['user_id', 'favorited_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
