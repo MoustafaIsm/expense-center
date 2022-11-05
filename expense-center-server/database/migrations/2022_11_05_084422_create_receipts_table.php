61e3c9f2878fb2d66099e96640fb12c0a452a5a3
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-05 10:46:19 +0200

    Backend => Created receipts table migration

---
+++ expense-center-server/database/migrations/2022_11_05_084422_create_receipts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('receipt_url');
+            $table->string('type');
+            $table->integer('amount');
+            $table->integer('sub_category_id')->references('id')->on('sub_categories');
+            $table->integer('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+};
