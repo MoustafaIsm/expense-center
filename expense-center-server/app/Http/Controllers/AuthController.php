fb1a5cbeedfedd90c275127d66152cdf86f3afc4
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-05 14:52:11 +0200

    Backend => Added the register function and not auth function

--- expense-center-server/app/Http/Controllers/AuthController.php
+++ expense-center-server/app/Http/Controllers/AuthController.php
@@ -27,6 +27,38 @@ class AuthController extends Controller {
             ]);
     }
 
+    public function register(Request $request) {
 
+        try {
+            // Add user to the database
+            $user = User::create([
+                'username' => 'Someone' . rand(1, 100000),
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'gender' => $request->gender,
+                'date_of_birth' => $request->date_of_birth,
+            ]);
+            // Login user
+            $token = Auth::login($user);
+            $user->token = $token;
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User created successfully',
+                'user' => $user
+            ]);
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $exception->errorInfo[2],
+            ], 400);
+        }
+    }
+
+    public function notAuth() {
+        return response()->json([
+            'status' => 'error',
+            'message' => "You are unauthorized.",
+        ], 401);
+    }
 
 }
