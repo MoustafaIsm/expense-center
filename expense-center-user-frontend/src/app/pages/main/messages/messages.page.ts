4531bcf22241cb5886a7da6a79ed948f58d947be
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-07 14:38:22 +0200

    Firebase => Moved the firebase databse variable to the service

--- expense-center-user-frontend/src/app/pages/main/messages/messages.page.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/messages.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { firebaseApp } from 'src/app/services/firebase';
-import { getDatabase } from 'firebase/database';
 
 @Component({
   selector: 'app-messages',
@@ -8,9 +6,6 @@ import { getDatabase } from 'firebase/database';
   styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
-
-  // Initialize Realtime Database and get a reference to the service
-  database = getDatabase(firebaseApp);
 
   constructor() { }
 
--- expense-center-user-frontend/src/app/services/chat/chat-service.service.ts
+++ expense-center-user-frontend/src/app/services/chat/chat-service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { firebaseApp } from 'src/app/services/firebase';
+import { getDatabase } from 'firebase/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
+
+  // Initialize Realtime Database and get a reference to the service
+  database = getDatabase(firebaseApp);
 
   constructor() { }
 }
