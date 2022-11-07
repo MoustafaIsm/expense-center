90d62e5a73a9066a2098ceab37f7fb7c3cbc91e2
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-07 14:15:38 +0200

    Firebase => Created a firebase serviece file

---
+++ expense-center-user-frontend/src/app/servieces/firebase/index.ts
@@ -0,0 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { firebaseConfiguration } from 'src/environments/environment';
+
+const firebaseConfig = firebaseConfiguration;
+
+export const app = initializeApp(firebaseConfig);
