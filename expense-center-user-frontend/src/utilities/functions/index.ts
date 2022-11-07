aafbf354cb5df81f20c5ca55e41c4c5baaf79e00
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-07 17:46:30 +0200

    Firebase => Moved some functions to the utilities file

--- expense-center-user-frontend/src/utilities/functions/index.ts
+++ expense-center-user-frontend/src/utilities/functions/index.ts
@@ -1,3 +1,6 @@
+import { ChatItem } from 'src/app/interfaces/ChatItem';
+import { Message } from 'src/app/interfaces/Message';
+
 export const getCurrentDate = (): string => {
   const today = new Date();
   const yyyy = today.getFullYear();
@@ -5,4 +8,29 @@ export const getCurrentDate = (): string => {
   const dd = today.getDate();
 
   return `${dd < 10 ? '0' + dd : dd}/${mm < 10 ? '0' + mm : mm}/${yyyy}`;
+};
+
+export const convertToChatItem = (data: any, id: string): ChatItem => {
+  const chatItem: ChatItem = {
+    id,
+    firstUserId: data.first_user_id,
+    secondUserId: data.second_user_id,
+    latestMessage: {
+      sentBy: data.latest_message.sender_id,
+      message: data.latest_message.message,
+      timeStamp: data.latest_message.timeStamp
+    },
+    messages: []
+  };
+  return chatItem;
+};
+
+export const convertToMessage = (data: any, id: string): Message => {
+  const message: Message = {
+    id,
+    sentBy: data.sentBy,
+    message: data.message,
+    timeStamp: data.timeStamp
+  };
+  return message;
 };
