5393fea9b5b38c60733cf29927ee8de948000aaf
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-07 22:16:19 +0200

    Linking => Created a user interface

---
+++ expense-center-user-frontend/src/app/interfaces/User.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  gender: string;
+  dateOfBirth: string;
+  profilePictureURL: string;
+  nbrOfChildern: number;
+  relationshipStatus: string;
+  chatEnabled: boolean;
+  educationFeild: string;
+  workFeild: string;
+  jobTitle: string;
+  income: number;
+  outcome: number;
+  yearlySalary: number;
+  livingLocationId: number;
+  token: string;
+}
