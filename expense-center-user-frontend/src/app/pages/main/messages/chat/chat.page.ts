e495862ae716d953cd95d85504486ba9db20a064
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-07 16:42:20 +0200

    Firebase => Made the chat page get the messages needed

--- expense-center-user-frontend/src/app/pages/main/messages/chat/chat.page.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/chat/chat.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Message } from 'src/app/interfaces/Message';
+import { ChatService } from 'src/app/services/chat/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  chatId: number;
+  mesaages: Message[] = [];
 
   message1 = {
     id: 1,
@@ -13,9 +18,12 @@ export class ChatPage implements OnInit {
   };
   id = 1;
 
-  constructor() { }
+  constructor(private activaitedRoute: ActivatedRoute, private chatService: ChatService) {
+    this.chatId = this.activaitedRoute.snapshot.params.id;
+  }
 
   ngOnInit() {
+    this.chatService.getChatMessages(this.chatId).subscribe((messages) => this.mesaages = messages);
   }
 
 }
