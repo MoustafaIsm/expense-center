5813923700655d4909b4822a01803e6be56289fd
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-08 09:34:28 +0200

    Linking => Added a get user functionin the porfile service

--- expense-center-user-frontend/src/app/services/profile/profile.service.ts
+++ expense-center-user-frontend/src/app/services/profile/profile.service.ts
@@ -1,9 +1,25 @@
+/* eslint-disable quote-props */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { corsHeaders, databaseURL } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    'Content-Type': 'application/json',
+    ...corsHeaders
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUser(id: number) {
+    return this.http.get(`${databaseURL}/user/get_user/${id}`, httpOptions);
+  }
 }
