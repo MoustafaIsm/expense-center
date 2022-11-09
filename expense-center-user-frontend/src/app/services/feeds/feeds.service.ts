1473a52b90d8723a1d2b0b5227cd7fff79c4c8c8
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-09 11:36:57 +0200

    Linking => Added a get feeds service

--- expense-center-user-frontend/src/app/services/feeds/feeds.service.ts
+++ expense-center-user-frontend/src/app/services/feeds/feeds.service.ts
@@ -1,9 +1,26 @@
+/* eslint-disable quote-props */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { corsHeaders, databaseURL } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    'Content-Type': 'application/json',
+    ...corsHeaders
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFeeds(): Observable<any> {
+    return this.http.get<any>(`${databaseURL}/user/get_feed`, httpOptions);
+  }
 }
