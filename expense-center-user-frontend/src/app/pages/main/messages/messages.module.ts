c11ab4bc0ae692a286ad53fe2b1c5617207c732b
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-11 12:26:30 +0200

    Linking => Added the empty state of the messages tab

--- expense-center-user-frontend/src/app/pages/main/messages/messages.module.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/messages.module.ts
@@ -1,3 +1,4 @@
+import { EmptyStateModule } from 'src/app/components/empty-state/empty-state.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -16,6 +17,7 @@ import { MessagesPage } from './messages.page';
     IonicModule,
     MessagesPageRoutingModule,
     MainPagesHeaderModule,
+    EmptyStateModule,
     ComponentsModule
   ],
   declarations: [MessagesPage]
