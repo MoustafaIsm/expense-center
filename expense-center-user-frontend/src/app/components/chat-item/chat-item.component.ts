1d36273fdd83ee511b26cc6f87ffbf8800e3443b
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-11 11:07:03 +0200

    Linking => Made the chat item get user data and display whats needed

--- expense-center-user-frontend/src/app/components/chat-item/chat-item.component.ts
+++ expense-center-user-frontend/src/app/components/chat-item/chat-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChatItem } from 'src/app/interfaces/ChatItem';
+import { User } from 'src/app/interfaces/User';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'app-chat-item',
@@ -10,14 +12,24 @@ import { ChatItem } from 'src/app/interfaces/ChatItem';
 export class ChatItemComponent implements OnInit {
   @Input() chatItem: ChatItem;
   userId: number = parseInt(localStorage.getItem('id'), 10);
+  otherUser: User;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private profileService: ProfileService) { }
 
   // TODO: Get user info with an api call
-  ngOnInit() { }
+  ngOnInit() {
+    this.getUser();
+  }
 
   openChat() {
     this.router.navigate(['main/messages/chat', this.chatItem.id]);
+  }
+
+  getUser() {
+    const id = this.userId === this.chatItem.firstUserId ? this.chatItem.secondUserId : this.chatItem.firstUserId;
+    this.profileService.getUser(id.toString()).subscribe(data => {
+      this.otherUser = data.user;
+    });
   }
 
 }
