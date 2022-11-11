b42c85a287bcf0f2327fcc31e17da5663199c2a7
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-11 12:42:47 +0200

    Linking => Added a search function in search service

--- expense-center-user-frontend/src/app/services/search/search.service.ts
+++ expense-center-user-frontend/src/app/services/search/search.service.ts
@@ -1,9 +1,25 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable quote-props */
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { databaseURL, corsHeaders } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  search(username: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+        ...corsHeaders
+      })
+    };
+    return this.http.get(databaseURL + 'user/search/username=' + username, httpOptions);
+  }
 }
