bdb4420103957d4c132050023ff584d11d174434
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-13 18:23:38 +0200

    Firebase-server => Initialized my firebase admin sdk

--- expense-center-firebase-server/functions/index.js
+++ expense-center-firebase-server/functions/index.js
@@ -1,9 +1,9 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const serviceAccount = require('./config/account-credentials.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://expense-center-013-default-rtdb.firebaseio.com"
+});
