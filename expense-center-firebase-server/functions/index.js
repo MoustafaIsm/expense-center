caf23ce58c1f258a8d9f157f1245e944b4ae2feb
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-13 18:30:02 +0200

    Firebase-server => Added the ability of the function get some user data

--- expense-center-firebase-server/functions/index.js
+++ expense-center-firebase-server/functions/index.js
@@ -15,5 +15,25 @@ exports.sendNotification = functions.database.ref("chats/{chatId}/messages/{mess
         const sentBy = snapshot.after.val().sentBy;
         const sentTo = snapshot.after.val().sentTo;
 
+        // Prepare promise to get the device notification tokens.
+        const sendToNotificationToken = admin.database()
+            .ref(`/users/${sentTo}/notificationToken`).once("value");
+        const sentByUsername = admin.database()
+            .ref(`/users/${sentBy}/username`).once("value");
+
+        // Get the device notification tokens.
+        const result = await Promise.all([sendToNotificationToken, sentByUsername]);
+
+        const tokenSnapshot = result[0];
+        const usernameSnapshot = result[1];
+
+        // Check if there are any device tokens.
+        if (!tokenSnapshot.exists()) {
+            return functions.logger.log("There are no notification tokens to send to.");
+        }
+        // Get token
+        const token = tokenSnapshot.val();
+        const username = usernameSnapshot.val();
+
 
     });
