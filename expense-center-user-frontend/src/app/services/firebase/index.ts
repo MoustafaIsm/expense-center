580134c8dff4ac94ea8499c50ba7cce20291fa05
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-13 18:22:33 +0200

    Firebase-server => Added firebase to the user frontend

--- expense-center-user-frontend/src/app/services/firebase/index.ts
+++ expense-center-user-frontend/src/app/services/firebase/index.ts
@@ -1,6 +1,14 @@
 import { initializeApp } from 'firebase/app';
+import { connectDatabaseEmulator, getDatabase } from 'firebase/database';
 import { firebaseConfiguration } from 'src/environments/environment';
 
 const firebaseConfig = firebaseConfiguration;
 
 export const firebaseApp = initializeApp(firebaseConfig);
+
+export const database = getDatabase(firebaseApp);
+// TODO: Remove this when deploying
+if (location.hostname === 'localhost') {
+  // Point to the RTDB emulator running on localhost.
+  connectDatabaseEmulator(database, 'localhost', 9000);
+}
