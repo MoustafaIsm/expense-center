2219d1ad38f1a44a0109419b9da769f3ecb67b51
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-13 18:29:11 +0200

    Firebase-server => Added a function that detects the change in the database

--- expense-center-firebase-server/functions/index.js
+++ expense-center-firebase-server/functions/index.js
@@ -7,3 +7,13 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://expense-center-013-default-rtdb.firebaseio.com"
 });
+
+
+exports.sendNotification = functions.database.ref("chats/{chatId}/messages/{messageId}")
+    .onWrite(async (snapshot) => {
+        const message = snapshot.after.val().message;
+        const sentBy = snapshot.after.val().sentBy;
+        const sentTo = snapshot.after.val().sentTo;
+
+
+    });
