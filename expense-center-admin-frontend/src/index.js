9a0fd5cd4386dcebed28579a4840c0250d73359b
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-14 09:22:54 +0200

    Admin-linking => Intializzed a query client in my app

--- expense-center-admin-frontend/src/index.js
+++ expense-center-admin-frontend/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import './fonts/Roboto/Roboto-Regular.ttf';
 import ModalProvider from 'mui-modal-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ModalProvider>
-        <App />
-    </ModalProvider>
+    <QueryClientProvider client={queryClient}>
+        {/* TODO: Remove the devtools when deploying */}
+        <ReactQueryDevtools initialIsOpen />
+        <ModalProvider>
+            <App />
+        </ModalProvider>
+    </QueryClientProvider>
 );
 
