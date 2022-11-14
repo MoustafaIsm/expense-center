103ccae4891ce480731be2892332c8b1e1b546d6
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-14 12:07:00 +0200

    Admin-linking => Added get userby id hook

--- expense-center-admin-frontend/src/query/users/index.js
+++ expense-center-admin-frontend/src/query/users/index.js
@@ -5,6 +5,13 @@ export const getBannedUsers = () => {
     return adminInstance.get('/users/get_banned_users').then(res => res.data.users);
 }
 
+export const getNotBannedUsers = () => {
+    return adminInstance.get('/users/get_not_banned_users');
+}
+
+export const getUserById = (id) => {
+    return adminInstance.get(`/users/get_user/${id}`);
+}
 
 export const useBannedUsers = () => useQuery({
     refetchOnWindowFocus: false,
@@ -12,3 +19,8 @@ export const useBannedUsers = () => useQuery({
     queryFn: () => getBannedUsers(),
 })
 
+export const useUserById = (id) => useQuery({
+    refetchOnWindowFocus: false,
+    queryKey: ['USER_BY_ID', id],
+    queryFn: () => getUserById(id),
+})
