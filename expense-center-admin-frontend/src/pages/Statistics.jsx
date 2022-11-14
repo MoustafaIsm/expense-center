f0f7f2082bde11e7dfe26d74971a74c3168168cb
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-14 14:59:33 +0200

    Admin-linking => Added the graph for most favorited users

--- expense-center-admin-frontend/src/pages/Statistics.jsx
+++ expense-center-admin-frontend/src/pages/Statistics.jsx
@@ -2,7 +2,7 @@ import Layout from "../components/layouts/Layout";
 import { STATISTICS } from '../utilities/constants';
 import StatisticsBarGraph from "../components/graphs/StatisticsBarGraph";
 import StatisticsLineGraph from "../components/graphs/StatisticsLineGraph";
-import { useMostClickedUsers } from '../query/statistics';
+import { useMostClickedUsers, useMostFavoritedUsers } from '../query/statistics';
 import { useEffect, useState } from "react";
 
 function Statistics() {
@@ -10,9 +10,12 @@ function Statistics() {
     // TODO: Send the data to the graphs through props
 
     const { data: mostClickedUsers, isSuccess: mostClickedUsersSuccess } = useMostClickedUsers();
+    const { data: mostFavoritedUsers, isSuccess: mostFavoritedUsersSuccess } = useMostFavoritedUsers();
 
     const [mostClickedLabels, setMostClickedLabels] = useState([]);
     const [mostClickedData, setMostClickedData] = useState([]);
+    const [mostFavoritedLabels, setMostFavoritedLabels] = useState([]);
+    const [mostFavoritedData, setMostFavoritedData] = useState([]);
 
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -22,7 +25,11 @@ function Statistics() {
             setMostClickedLabels(mostClickedUsers.map(user => user.username));
             setMostClickedData(mostClickedUsers.map(user => user.nbr_of_clicks));
         }
-    }, [mostClickedUsers, mostClickedUsersSuccess]);
+        if (mostFavoritedUsersSuccess) {
+            setMostFavoritedLabels(mostFavoritedUsers.map(user => user.favorited_info.username));
+            setMostFavoritedData(mostFavoritedUsers.map(user => user.total));
+        }
+    }, [mostClickedUsers, mostClickedUsersSuccess, mostFavoritedUsers, mostFavoritedUsersSuccess]);
 
     return (
         <Layout title={STATISTICS}>
@@ -32,6 +39,11 @@ function Statistics() {
                     label={'Clicks'}
                     labels={mostClickedLabels}
                     userData={mostClickedData} />
+                <StatisticsBarGraph
+                    title="Most favorited users"
+                    label={'Favorites'}
+                    labels={mostFavoritedLabels}
+                    userData={mostFavoritedData} />
                 {
                     array.map((item, index) => {
                         return (
