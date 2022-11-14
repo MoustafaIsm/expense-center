0aea2b4deeea080a8192ed0ec973092bbb712f76
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-14 14:50:23 +0200

    Admin-linking => Added a get most favorited users hook

--- expense-center-admin-frontend/src/query/statistics/index.js
+++ expense-center-admin-frontend/src/query/statistics/index.js
@@ -5,9 +5,20 @@ export const getMostClickedUsers = () => {
     return adminInstance.get('/statistics/get_most_clicked_users').then(res => res.data.users);
 }
 
+export const getMostFavoritedUsers = () => {
+    return adminInstance.get('/statistics/get_most_favorited_users').then(res => res.data.users);
+}
+
 export const useMostClickedUsers = () => useQuery({
     refetchOnWindowFocus: false,
     queryKey: ['MOST_CLICKED_USERS'],
     queryFn: () => getMostClickedUsers(),
+    placeholderData: [],
+})
+
+export const useMostFavoritedUsers = () => useQuery({
+    refetchOnWindowFocus: false,
+    queryKey: ['MOST_FAVORITED_USERS'],
+    queryFn: () => getMostFavoritedUsers(),
     placeholderData: [],
 })
