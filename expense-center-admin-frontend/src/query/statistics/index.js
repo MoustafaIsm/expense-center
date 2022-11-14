b851366cc4323e24313275aacb6061a0048e4e6e
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-14 15:15:04 +0200

    Admin-linking => Added a get savings hook

--- expense-center-admin-frontend/src/query/statistics/index.js
+++ expense-center-admin-frontend/src/query/statistics/index.js
@@ -17,6 +17,10 @@ export const getOutcomes = () => {
     return adminInstance.get('/statistics/get_outcomes').then(res => res.data.outcomes);
 }
 
+export const getSavings = () => {
+    return adminInstance.get('/statistics/get_savings').then(res => res.data.savings);
+}
+
 export const useMostClickedUsers = () => useQuery({
     refetchOnWindowFocus: false,
     queryKey: ['MOST_CLICKED_USERS'],
@@ -42,5 +46,12 @@ export const useOutcomes = () => useQuery({
     refetchOnWindowFocus: false,
     queryKey: ['OUTCOMES'],
     queryFn: () => getOutcomes(),
+    placeholderData: [],
+})
+
+export const useSavings = () => useQuery({
+    refetchOnWindowFocus: false,
+    queryKey: ['SAVINGS'],
+    queryFn: () => getSavings(),
     placeholderData: [],
 })
