317926a285c75b6e09098c4534de03fec8e7b548
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-15 16:07:05 +0200

    Linking-2 => Cleaned some files in the project

--- expense-center-admin-frontend/src/query/users/index.js
+++ expense-center-admin-frontend/src/query/users/index.js
@@ -1,21 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { adminInstance } from '../axios';
+import { handleError } from '../../utilities/functions';
 
 export const getBannedUsers = () => {
     return adminInstance.get('/users/get_banned_users')
         .then(res => res.data.users)
-        .catch(error => console.log(error.response));
+        .catch(error => handleError(error.response));
 }
 
 export const getNotBannedUsers = () => {
     return adminInstance.get('/users/get_not_banned_users')
         .then(res => res.data.users)
-        .catch(error => console.log(error.response));
+        .catch(error => handleError(error.response));
 }
 
 export const getUserById = (id) => {
     return adminInstance.get(`/users/get_user/${id}`)
-        .catch(error => console.log(error.response));
+        .catch(error => handleError(error.response));
 }
 
 export const useBannedUsers = () => useQuery({
--- expense-center-admin-frontend/src/utilities/functions.js
+++ expense-center-admin-frontend/src/utilities/functions.js
@@ -1,3 +1,5 @@
+import { faTableColumns, faShapes, faChartSimple, faMessage } from '@fortawesome/free-solid-svg-icons';
+import { PAGE_TYPES } from './constants';
 
 export const handleError = (error) => {
     if (error.status === 401) {
@@ -11,4 +13,25 @@ export const handleError = (error) => {
 export const getCurrentDate = () => {
     const date = new Date();
     return { day: date.getDate(), month: date.getMonth() + 1, year: date.getFullYear() };
+}
+
+export const getPage = (type) => {
+    let icon = '';
+    switch (type) {
+        case PAGE_TYPES[0]:
+            icon = faTableColumns;
+            break;
+        case PAGE_TYPES[1]:
+            icon = faShapes;
+            break;
+        case PAGE_TYPES[2]:
+            icon = faChartSimple;
+            break;
+        case PAGE_TYPES[3]:
+            icon = faMessage;
+            break;
+        default:
+            break;
+    }
+    return icon;
 }
