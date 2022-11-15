c4d41e50caf035b59c6049243e47f17f3235864d
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-15 15:23:56 +0200

    Linking-2 => Changed the admin statistics way of handling the use queries

--- expense-center-admin-frontend/src/pages/Statistics.jsx
+++ expense-center-admin-frontend/src/pages/Statistics.jsx
@@ -3,89 +3,47 @@ import { STATISTICS } from '../utilities/constants';
 import StatisticsBarGraph from "../components/graphs/StatisticsBarGraph";
 import StatisticsLineGraph from "../components/graphs/StatisticsLineGraph";
 import { useMostClickedUsers, useMostFavoritedUsers, useIncomes, useOutcomes, useSavings } from '../query/statistics';
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function Statistics() {
 
-    const { data: mostClickedUsers, isSuccess: mostClickedUsersSuccess } = useMostClickedUsers();
-    const { data: mostFavoritedUsers, isSuccess: mostFavoritedUsersSuccess } = useMostFavoritedUsers();
-    const { data: incomes, isSuccess: incomesSuccess } = useIncomes();
-    const { data: outcomes, isSuccess: outcomesSuccess } = useOutcomes();
-    const { data: savings, isSuccess: savingsSuccess } = useSavings();
+    const { data: mostClickedUsers } = useMostClickedUsers();
+    const { data: mostFavoritedUsers } = useMostFavoritedUsers();
+    const { data: incomes } = useIncomes();
+    const { data: outcomes } = useOutcomes();
+    const { data: savings } = useSavings();
 
-    const [mostClickedLabels, setMostClickedLabels] = useState([]);
-    const [mostClickedData, setMostClickedData] = useState([]);
-    const [mostFavoritedLabels, setMostFavoritedLabels] = useState([]);
-    const [mostFavoritedData, setMostFavoritedData] = useState([]);
-    const [incomesLabels, setIncomesLabels] = useState([]);
-    const [incomesData, setIncomesData] = useState([]);
-    const [outcomesLabels, setOutcomesLabels] = useState([]);
-    const [outcomesData, setOutcomesData] = useState([]);
-    const [savingsLabels, setSavingsLabels] = useState([]);
-    const [savingsData, setSavingsData] = useState([]);
+    const dataFromServer = [mostClickedUsers, mostFavoritedUsers, incomes, outcomes, savings];
 
     useEffect(() => {
-        if (mostClickedUsersSuccess) {
-            setMostClickedLabels(mostClickedUsers.map(user => user.username));
-            setMostClickedData(mostClickedUsers.map(user => user.nbr_of_clicks));
-        }
-        if (mostFavoritedUsersSuccess) {
-            setMostFavoritedLabels(mostFavoritedUsers.map(user => user.favorited_info.username));
-            setMostFavoritedData(mostFavoritedUsers.map(user => user.total));
-        }
-        if (incomesSuccess) {
-            setIncomesLabels(incomes.map(income => income.month + '-' + income.year));
-            setIncomesData(incomes.map(income => income.total));
-        }
-        if (outcomesSuccess) {
-            setOutcomesLabels(outcomes.map(outcome => outcome.month + '-' + outcome.year));
-            setOutcomesData(outcomes.map(outcome => outcome.total));
-        }
-        if (savingsSuccess) {
-            setSavingsLabels(savings.map(saving => saving.month + '-' + saving.year));
-            setSavingsData(savings.map(saving => saving.total));
-        }
-    }, [
-        mostClickedUsers,
-        mostClickedUsersSuccess,
-        mostFavoritedUsers,
-        mostFavoritedUsersSuccess,
-        incomes,
-        incomesSuccess,
-        outcomes,
-        outcomesSuccess,
-        savings,
-        savingsSuccess
-    ]);
+
+    }, []);
 
     return (
         <Layout title={STATISTICS}>
             <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-                <StatisticsBarGraph
-                    title="Most clicked users"
-                    label={'Clicks'}
-                    labels={mostClickedLabels}
-                    userData={mostClickedData} />
-                <StatisticsBarGraph
-                    title="Most favorited users"
-                    label={'Favorites'}
-                    labels={mostFavoritedLabels}
-                    userData={mostFavoritedData} />
-                <StatisticsLineGraph
-                    title="Incomes"
-                    label={'Incomes'}
-                    labels={incomesLabels}
-                    statData={incomesData} />
-                <StatisticsLineGraph
-                    title="Outcomes"
-                    label={'Outcomes'}
-                    labels={outcomesLabels}
-                    statData={outcomesData} />
-                <StatisticsLineGraph
-                    title="Savings"
-                    label={'Savings'}
-                    labels={savingsLabels}
-                    statData={savingsData} />
+                {
+                    dataFromServer.map((data, index) => {
+                        return (
+                            <div key={index}>
+                                {
+                                    data.type === 'bar' ?
+                                        <StatisticsBarGraph
+                                            title={data.title}
+                                            label={data.label}
+                                            labels={data.labels}
+                                            userData={data.dataset} />
+                                        :
+                                        <StatisticsLineGraph
+                                            title={data.title}
+                                            label={data.label}
+                                            labels={data.labels}
+                                            statData={data.dataset} />
+                                }
+                            </div>
+                        );
+                    })
+                }
             </div>
         </Layout>
     )
--- expense-center-admin-frontend/src/query/statistics/index.js
+++ expense-center-admin-frontend/src/query/statistics/index.js
@@ -37,6 +37,15 @@ export const useMostClickedUsers = () => useQuery({
     queryKey: ['MOST_CLICKED_USERS'],
     queryFn: () => getMostClickedUsers(),
     placeholderData: [],
+    select: data => {
+        return {
+            type: 'bar',
+            title: 'Most clicked users',
+            label: 'Clicks',
+            labels: data.map(user => user.username),
+            dataset: data.map(user => user.nbr_of_clicks),
+        };
+    }
 })
 
 export const useMostFavoritedUsers = () => useQuery({
@@ -44,6 +53,15 @@ export const useMostFavoritedUsers = () => useQuery({
     queryKey: ['MOST_FAVORITED_USERS'],
     queryFn: () => getMostFavoritedUsers(),
     placeholderData: [],
+    select: data => {
+        return {
+            type: 'bar',
+            title: 'Most favorited users',
+            label: 'Favorites',
+            labels: data.map(user => user.favorited_info.username),
+            dataset: data.map(user => user.total),
+        };
+    }
 })
 
 export const useIncomes = () => useQuery({
@@ -51,6 +69,15 @@ export const useIncomes = () => useQuery({
     queryKey: ['INCOMES'],
     queryFn: () => getIncomes(),
     placeholderData: [],
+    select: data => {
+        return {
+            type: 'line',
+            title: 'Incomes',
+            label: '$',
+            labels: data.map(income => income.month + '-' + income.year),
+            dataset: data.map(income => income.total),
+        };
+    }
 })
 
 export const useOutcomes = () => useQuery({
@@ -58,6 +85,15 @@ export const useOutcomes = () => useQuery({
     queryKey: ['OUTCOMES'],
     queryFn: () => getOutcomes(),
     placeholderData: [],
+    select: data => {
+        return {
+            type: 'line',
+            title: 'Outcomes',
+            label: '$',
+            labels: data.map(outcome => outcome.month + '-' + outcome.year),
+            dataset: data.map(outcome => outcome.total),
+        };
+    }
 })
 
 export const useSavings = () => useQuery({
@@ -65,4 +101,16 @@ export const useSavings = () => useQuery({
     queryKey: ['SAVINGS'],
     queryFn: () => getSavings(),
     placeholderData: [],
+    select: data => {
+        return {
+            type: 'line',
+            title: 'Savings',
+            label: '$',
+            labels: data.map(saving => saving.month + '-' + saving.year),
+            dataset: data.map(saving => saving.total),
+        };
+    },
+    onSuccess: data => {
+        console.log(data);
+    }
 })
