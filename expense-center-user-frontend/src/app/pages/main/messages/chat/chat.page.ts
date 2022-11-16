f5628de0874ec93492def3d9ca7a5eef351ebfd8
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-16 20:56:35 +0200

    Linking-2 => Handled some errors in the messages page

--- expense-center-user-frontend/src/app/pages/main/messages/chat/chat.page.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/chat/chat.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Message } from 'src/app/interfaces/Message';
 import { ChatService } from 'src/app/services/chat/chat.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat',
@@ -19,7 +20,8 @@ export class ChatPage implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private chatService: ChatService
+    private chatService: ChatService,
+    private toastController: ToastController,
   ) {
     this.chatId = this.activatedRoute.snapshot.params.id;
     const navigation = this.router.getCurrentNavigation();
@@ -27,12 +29,23 @@ export class ChatPage implements OnInit {
   }
 
   ngOnInit() {
-    this.chatService.getChatMessages(this.chatId).subscribe((messages) => this.messages = messages);
+    this.chatService.getChatMessages(this.chatId).subscribe(
+      (messages) => this.messages = messages,
+      (error) => this.presentToast('Something went wrong'));
   }
 
   sendMessage() {
     this.chatService.sendMessage(this.chatId, this.userId, this.user.id, this.newMessage, (this.messages.length + 1).toString());
     this.newMessage = '';
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+    });
+
+    await toast.present();
   }
 
 }
--- expense-center-user-frontend/src/app/pages/main/messages/messages.page.ts
+++ expense-center-user-frontend/src/app/pages/main/messages/messages.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { ChatItem } from 'src/app/interfaces/ChatItem';
 import { ChatService } from '../../../services/chat/chat.service';
 
@@ -11,10 +12,21 @@ export class MessagesPage implements OnInit {
   chats: ChatItem[] = [];
   userId: number = parseInt(localStorage.getItem('id'), 10);
 
-  constructor(private chatService: ChatService) { }
+  constructor(private chatService: ChatService, private toastController: ToastController) { }
 
   ngOnInit(): void {
-    this.chatService.getChatItems(this.userId).subscribe((chats) => this.chats = chats);
+    this.chatService.getChatItems(this.userId).subscribe(
+      (chats) => this.chats = chats,
+      (error) => this.presentToast('Something went wrong'));
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+    });
+
+    await toast.present();
   }
 
 }
