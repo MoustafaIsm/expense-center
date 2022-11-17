457d9b5f79e91761c90b65f4bd7951fe829df09d
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-17 08:38:36 +0200

    Linking-2 => Unsubscribed from the services in the home page

--- expense-center-user-frontend/src/app/pages/main/home/home.page.ts
+++ expense-center-user-frontend/src/app/pages/main/home/home.page.ts
@@ -1,18 +1,20 @@
 import { FavoritesService } from 'src/app/services/favorites/favorites.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/User';
 import { FeedsService } from 'src/app/services/feeds/feeds.service';
 import { presentToast } from 'src/utilities/functions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   feeds: User[] = [];
   isModalOpen = false;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private router: Router,
@@ -22,6 +24,12 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
     this.getFeeds();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => {
+      subscription.unsubscribe();
+    });
   }
 
   openSearch() {
@@ -36,7 +44,7 @@ export class HomePage implements OnInit {
   }
 
   openUserProfile(id: number) {
-    this.feedsService.increaseClickCount({ id }).subscribe(
+    const temp = this.feedsService.increaseClickCount({ id }).subscribe(
       data => {
         this.router.navigate(['main/user-profile', id]);
       }, (error) => {
@@ -47,47 +55,54 @@ export class HomePage implements OnInit {
           presentToast('Something went wrong');
         }
       });
+    this.subscriptions.push(temp);
   }
 
   getFeeds() {
-    this.feedsService.getFeeds().subscribe((data) => {
-      this.feeds = data.users;
-    }, (error) => {
-      if (error.status === 401) {
-        presentToast('Please login to view feeds');
-        this.router.navigate(['login']);
-      } else {
-        presentToast('Something went wrong');
-      }
-    });
+    const temp = this.feedsService.getFeeds().subscribe(
+      (data) => {
+        this.feeds = data.users;
+      }, (error) => {
+        if (error.status === 401) {
+          presentToast('Please login to view feeds');
+          this.router.navigate(['login']);
+        } else {
+          presentToast('Something went wrong');
+        }
+      });
+    this.subscriptions.push(temp);
   }
 
   unFavoriteUser(id: number) {
-    this.favoriteService.unFavoriteUser(id).subscribe(data => {
-      presentToast('User removed from favorites');
-      this.feeds.find(user => user.id === id).isFavorited = false;
-    }, (error) => {
-      if (error.status === 401) {
-        presentToast('Please login to view feeds');
-        this.router.navigate(['login']);
-      } else {
-        presentToast('Something went wrong');
-      }
-    });
+    const temp = this.favoriteService.unFavoriteUser(id).subscribe(
+      data => {
+        presentToast('User removed from favorites');
+        this.feeds.find(user => user.id === id).isFavorited = false;
+      }, error => {
+        if (error.status === 401) {
+          presentToast('Please login to view feeds');
+          this.router.navigate(['login']);
+        } else {
+          presentToast('Something went wrong');
+        }
+      });
+    this.subscriptions.push(temp);
   }
 
   favoriteUser(id: number) {
-    this.favoriteService.favoriteUser(id).subscribe(data => {
-      presentToast('User added to favorites');
-      this.feeds.find(user => user.id === id).isFavorited = true;
-    }, (error) => {
-      if (error.status === 401) {
-        presentToast('Please login to view feeds');
-        this.router.navigate(['login']);
-      } else {
-        presentToast('Something went wrong');
-      }
-    });
+    const temp = this.favoriteService.favoriteUser(id).subscribe(
+      data => {
+        presentToast('User added to favorites');
+        this.feeds.find(user => user.id === id).isFavorited = true;
+      }, (error) => {
+        if (error.status === 401) {
+          presentToast('Please login to view feeds');
+          this.router.navigate(['login']);
+        } else {
+          presentToast('Something went wrong');
+        }
+      });
+    this.subscriptions.push(temp);
   }
 
 }
