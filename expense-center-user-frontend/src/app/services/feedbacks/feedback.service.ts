fd8aa202b1b729173c1e3074a6ac115a4f999cff
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-17 20:11:27 +0200

    Final => Added the send message function in the feedback service

--- expense-center-user-frontend/src/app/services/feedbacks/feedback.service.ts
+++ expense-center-user-frontend/src/app/services/feedbacks/feedback.service.ts
@@ -1,9 +1,24 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { databaseURL, corsHeaders } from '../../../environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    ...corsHeaders
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendFeedback(message: string): Observable<any> {
+    return this.http.post<any>(`${databaseURL}/feedback/send_feedback`, { message }, httpOptions);
+  }
 }
