ae66ea8c5880a24b48cf16bfda42c734fe9e6d3a
Moustafa Ismail <moustafaism013@hotmail.com>
2022-11-18 10:44:35 +0200

    Final => Rerouted the user to the main page if he was already logged in

--- expense-center-user-frontend/src/app/app.component.ts
+++ expense-center-user-frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { getUser } from 'src/app/state/selectors';
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { User } from './interfaces/User';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() { }
+
+  constructor(private platform: Platform, private router: Router, private store: Store) {
+    this.platform.ready().then(() => {
+      const id = localStorage.getItem('id');
+      if (id) {
+        this.router.navigate(['main']);
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
